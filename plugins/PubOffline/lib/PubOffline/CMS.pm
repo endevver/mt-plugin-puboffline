bb678707166e95d1fe898c0ffc27268eb02638a8
Dan Wolfgang <dan@uinnovations.com>
2011-11-30 15:00:29 -0500

    The Jumpstart should handle static content as well as assets.

--- plugins/PubOffline/lib/PubOffline/CMS.pm
+++ plugins/PubOffline/lib/PubOffline/CMS.pm
@@ -18,37 +18,47 @@ sub settings {
 # Use a popup window for the process of "jumpstarting" assets -- sending them
 # all to the PQ to be handled for offline use. This is used both for the
 # initial loading of the jumpstart dialog, and to do the actual jumpstart
-sub jumpstart_assets {
+sub jumpstart {
     my $app     = shift;
     my ($param) = @_;
     my $q       = $app->query;
+    my $blog_id = $q->param('blog_id');
     my $plugin  = MT->component('PubOffline');
 
     # We want to give up if PubOffline was not enabled on this blog.
     $param->{enabled} = $plugin->get_config_value(
         'enable_puboffline',
-        'blog:' . $q->param('blog_id')
+        'blog:' . $blog_id
     );
 
     # If the user has clicked "Jumpstart" to start the process, do it!
     if ( $q->param('jumpstart') ) {
+
+        require PubOffline::Plugin;
+
+        # Jumpstart assets
         my $iter = MT->model('asset')->load_iter({
-            blog_id => $q->param('blog_id'),
+            blog_id => $blog_id,
             class   => '*', # Grab all assets in this blog
         });
 
-        require PubOffline::Plugin;
-        
         while ( my $asset = $iter->() ) {
+            # Move on if this asset doesn't have a file path (it's not a
+            # file-based asset) or if the asset file doesn't exist.
+            next if !$asset->file_path || !-e $asset->file_path;
+
             PubOffline::Plugin::_create_asset_handling_job( $asset );
         }
+
+        # Jumpstart static files
+        PubOffline::Plugin::_create_static_handling_jobs( $blog_id );
 
         return $plugin->load_tmpl( 'dialog/close.tmpl' );
     }
 
-    $param->{blog_id} = $q->param('blog_id');
+    $param->{blog_id} = $blog_id;
 
-    return $plugin->load_tmpl( 'dialog/jumpstart_assets.tmpl', $param );
+    return $plugin->load_tmpl( 'dialog/jumpstart.tmpl', $param );
 }
 
 1;
