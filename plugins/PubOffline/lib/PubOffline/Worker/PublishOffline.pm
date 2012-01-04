af5afee22dd85966a6afb6b7b380217a0eb65465
Dan Wolfgang <dan@uinnovations.com>
2012-01-04 16:07:07 -0500

    Index template's output path is modified in _rebuild_indexes to avoid any blog site_path collision.

--- plugins/PubOffline/lib/PubOffline/Worker/PublishOffline.pm
+++ plugins/PubOffline/lib/PubOffline/Worker/PublishOffline.pm
@@ -9,6 +9,7 @@ use MT::FileInfo;
 use MT::PublishOption;
 use MT::Util qw( log_time );
 use PubOffline::Util qw( get_output_path path_exists );
+use File::Basename;
 
 sub keep_exit_status_for { 1 }
 
@@ -144,9 +145,8 @@ sub max_retries { 0 }
 sub retry_delay { 60 }
 
 # This is lifted from MT::WeblogPublisher, with a few changes.
-# Notably, when publishing index files the blog site path needs to be reset to
-# the offline path; when publishing archives the archive root needs to be set
-# to the offline path instead of the site path, also.
+# Notably, when publishing index files we need to direct them to our version
+# of _rebuild_indexes so that they are published to the right location.
 sub _rebuild_from_fileinfo {
     my $pub = MT::WeblogPublisher->new();
     my ($fi) = shift;
@@ -175,26 +175,16 @@ sub _rebuild_from_fileinfo {
     # Index templates get handled a little differently than archives.
     if ( $at eq 'index' ) {
         my $tmpl = MT->model('template')->load( $fi->template_id );
-
-        # Pass the blog object to rebuild_indexes with an updated site_path,
-        # reflecting the desired puboffline location. Save the original site
-        # path so that we can reset it later.
         my $blog = MT->model('blog')->load( $fi->blog_id );
-        my $saved_site_path = $blog->site_path;
-        $blog->site_path( $output_file_path );
 
-        $pub->rebuild_indexes(
+        # Use our _rebuild_indexes (rather than Weblog::Publisher's) so that
+        # the output path can be modified as necessary.
+        _rebuild_indexes(
             Blog     => $blog,
             Template => $tmpl,
             FileInfo => $fi,
             Force    => 1,
         ) or return;
-
-        # Reset to the original site path. If a non-
-        # PubOffline::Worker::PublishOffline worker runs after this (such as 
-        # MT::Worker::Publish) then we want it to have the "real" path to work
-        # with so that it can publish properly, too.
-        $blog->site_path( $saved_site_path );
 
         return 1;
     }
@@ -251,6 +241,272 @@ sub _rebuild_from_fileinfo {
 
     return 1;
 }
+
+# This is lifted from MT::WeblogPublisher, with a few changes.
+# The offline path needs to be fed when republishing index templates.
+sub _rebuild_indexes {
+    #my $mt    = shift;
+    my $mt = MT->instance;
+    my %param = @_;
+    require MT::Template;
+    require MT::Template::Context;
+    require MT::Entry;
+
+    my $blog;
+    $blog = $param{Blog}
+        if defined $param{Blog};
+    if (!$blog && defined $param{BlogID}) {
+        my $blog_id = $param{BlogID};
+        $blog = MT::Blog->load($blog_id)
+          or return $mt->error(
+            MT->translate(
+                "Load of blog '[_1]' failed: [_2]", $blog_id,
+                MT::Blog->errstr
+            )
+          );
+    }
+    my $tmpl = $param{Template};
+    if ($tmpl && (!$blog || $blog->id != $tmpl->blog_id)) {
+        $blog = MT::Blog->load( $tmpl->blog_id );
+    }
+
+    return $mt->error(
+        MT->translate(
+            "Blog, BlogID or Template param must be specified.")
+    ) unless $blog;
+
+    return 1 if $blog->is_dynamic;
+    my $iter;
+    if ($tmpl) {
+        my $i = 0;
+        $iter = sub { $i++ < 1 ? $tmpl : undef };
+    }
+    else {
+        $iter = MT::Template->load_iter(
+            {
+                type    => 'index',
+                blog_id => $blog->id
+            }
+        );
+    }
+    my $force = $param{Force};
+
+    local *FH;
+
+    # Use the offline site path that was previously set for this blog.
+    #my $site_root = $blog->site_path;
+    my $site_root = get_output_path({ blog_id => $blog->id });
+
+
+    return $mt->error(
+        MT->translate("You did not set your blog publishing path") )
+      unless $site_root;
+    my $fmgr = $blog->file_mgr;
+    while ( my $tmpl = $iter->() ) {
+        ## Skip index templates that the user has designated not to be
+        ## rebuilt automatically. We need to do the defined-ness check
+        ## because we added the flag in 2.01, and for templates saved
+        ## before that time, the rebuild_me flag will be undefined. But
+        ## we assume that these templates should be rebuilt, since that
+        ## was the previous behavior.
+        ## Note that dynamic templates do need to be "rebuilt"--the
+        ## FileInfo table needs to be maintained.
+        if ( !$tmpl->build_dynamic && !$force ) {
+            next if ( defined $tmpl->rebuild_me && !$tmpl->rebuild_me );
+        }
+        next if ( defined $tmpl->build_type && !$tmpl->build_type );
+
+        my $file = $tmpl->outfile;
+        $file = '' unless defined $file;
+        if ( $tmpl->build_dynamic && ( $file eq '' ) ) {
+            next;
+        }
+        return $mt->error(
+            MT->translate(
+                "Template '[_1]' does not have an Output File.",
+                $tmpl->name
+            )
+        ) unless $file ne '';
+        my $url = join( '/', $blog->site_url, $file );
+        unless ( File::Spec->file_name_is_absolute($file) ) {
+            $file = File::Spec->catfile( $site_root, $file );
+        }
+
+        # Everything from here out is identical with rebuild_file
+        my ($rel_url) = ( $url =~ m|^(?:[^:]*\:\/\/)?[^/]*(.*)| );
+        $rel_url =~ s|//+|/|g;
+        ## Untaint. We have to assume that we can trust the user's setting of
+        ## the site_path and the template outfile.
+        ($file) = $file =~ /(.+)/s;
+        my $finfo = $param{FileInfo};  # available for single template calls
+        unless ( $finfo ) {
+            require MT::FileInfo;
+            my @finfos = MT::FileInfo->load(
+                {
+                    blog_id     => $tmpl->blog_id,
+                    template_id => $tmpl->id
+                }
+            );
+            if (   ( scalar @finfos == 1 )
+                && ( $finfos[0]->file_path eq $file )
+                && ( ( $finfos[0]->url || '' ) eq $rel_url ) )
+            {
+                $finfo = $finfos[0];
+            }
+            else {
+                foreach (@finfos) { $_->remove(); }
+                $finfo = MT::FileInfo->set_info_for_url(
+                    $rel_url, $file, 'index',
+                    {
+                        Blog     => $tmpl->blog_id,
+                        Template => $tmpl->id,
+                    }
+                  )
+                  || die "Couldn't create FileInfo because " . MT::FileInfo->errstr;
+            }
+        }
+        if ( $tmpl->build_dynamic ) {
+            rename( $file, $file . ".static" );
+
+            ## If the FileInfo is set to static, flip it to virtual.
+            if ( !$finfo->virtual ) {
+                $finfo->virtual(1);
+                $finfo->save();
+            }
+        }
+
+        next if ( $tmpl->build_dynamic );
+        next unless ( $tmpl->build_type );
+
+        ## We're not building dynamically, so if the FileInfo is currently
+        ## set as dynamic (virtual), change it to static.
+        if ( $finfo && $finfo->virtual ) {
+            $finfo->virtual(0);
+            $finfo->save();
+        }
+
+        my $timer = MT->get_timer;
+        if ($timer) {
+            $timer->pause_partial;
+        }
+        local $timer->{elapsed} = 0 if $timer;
+
+        my $ctx = MT::Template::Context->new;
+        next
+          unless (
+            MT->run_callbacks(
+                'build_file_filter',
+                Context      => $ctx,
+                context      => $ctx,
+                ArchiveType  => 'index',
+                archive_type => 'index',
+                Blog         => $blog,
+                blog         => $blog,
+                FileInfo     => $finfo,
+                file_info    => $finfo,
+                Template     => $tmpl,
+                template     => $tmpl,
+                File         => $file,
+                file         => $file,
+                force        => $force,
+            )
+          );
+        $ctx->stash( 'blog', $blog );
+
+        require MT::Request;
+        MT::Request->instance->cache('build_template', $tmpl);
+
+        my $html = $tmpl->build($ctx);
+        unless (defined $html) {
+            $timer->unpause if $timer;
+            return $mt->error( $tmpl->errstr );
+        }
+
+        my $orig_html = $html;
+        MT->run_callbacks(
+            'build_page',
+            Context      => $ctx,
+            context      => $ctx,
+            Blog         => $blog,
+            blog         => $blog,
+            FileInfo     => $finfo,
+            file_info    => $finfo,
+            ArchiveType  => 'index',
+            archive_type => 'index',
+            RawContent   => \$orig_html,
+            raw_content  => \$orig_html,
+            Content      => \$html,
+            content      => \$html,
+            BuildResult  => \$orig_html,
+            build_result => \$orig_html,
+            Template     => $tmpl,
+            template     => $tmpl,
+            File         => $file,
+            file         => $file
+        );
+
+        ## First check whether the content is actually changed. If not,
+        ## we won't update the published file, so as not to modify the mtime.
+        next unless $fmgr->content_is_updated( $file, \$html );
+
+        ## Determine if we need to build directory structure,
+        ## and build it if we do. DirUmask determines
+        ## directory permissions.
+        require File::Spec;
+        my $path = dirname($file);
+        $path =~ s!/$!!
+          unless $path eq '/';    ## OS X doesn't like / at the end in mkdir().
+        unless ( $fmgr->exists($path) ) {
+            if (! $fmgr->mkpath($path) ) {
+                $timer->unpause if $timer;
+                return $mt->trans_error( "Error making path '[_1]': [_2]",
+                    $path, $fmgr->errstr );
+            }
+        }
+
+        ## Update the published file.
+        my $use_temp_files = !$mt->{NoTempFiles};
+        my $temp_file = $use_temp_files ? "$file.new" : $file;
+        unless (defined( $fmgr->put_data( $html, $temp_file ) )) {
+            $timer->unpause if $timer;
+            return $mt->trans_error( "Writing to '[_1]' failed: [_2]",
+                $temp_file, $fmgr->errstr );
+        }
+        if ($use_temp_files) {
+            if (!$fmgr->rename( $temp_file, $file )) {
+                $timer->unpause if $timer;
+                return $mt->trans_error( "Renaming tempfile '[_1]' failed: [_2]",
+                    $temp_file, $fmgr->errstr );
+            }
+        }
+        MT->run_callbacks(
+            'build_file',
+            Context      => $ctx,
+            context      => $ctx,
+            ArchiveType  => 'index',
+            archive_type => 'index',
+            FileInfo     => $finfo,
+            file_info    => $finfo,
+            Blog         => $blog,
+            blog         => $blog,
+            RawContent   => \$orig_html,
+            raw_content  => \$orig_html,
+            Content      => \$html,
+            content      => \$html,
+            BuildResult  => \$orig_html,
+            build_result => \$orig_html,
+            Template     => $tmpl,
+            template     => $tmpl,
+            File         => $file,
+            file         => $file
+        );
+
+        $timer->mark("total:rebuild_indexes[template_id:" . $tmpl->id . ";file:$file]")
+            if $timer;
+    }
+    1;
+}
+
 
 1;
 
